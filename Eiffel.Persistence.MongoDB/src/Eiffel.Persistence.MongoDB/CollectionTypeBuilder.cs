13bfc08c79bb6644cf7dce2f90966cab2b855fab
efeozyer <efeozyer@yandex.com>
2021-03-03 07:36:53 +0300

    parameters renamed

--- Eiffel.Persistence.MongoDB/src/Eiffel.Persistence.MongoDB/CollectionTypeBuilder.cs
+++ Eiffel.Persistence.MongoDB/src/Eiffel.Persistence.MongoDB/CollectionTypeBuilder.cs
@@ -168,29 +168,29 @@ namespace Eiffel.Persistence.MongoDB
 
     internal static class ExpressionExtensions
     {
-        internal static Expression<Func<T, bool>> CombineWith<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        internal static Expression<Func<T, bool>> CombineWith<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
         {
-            ParameterExpression parameter1 = expr1.Parameters[0];
-            var visitor = new ReplaceParameterVisitor(expr2.Parameters[0], parameter1);
-            var body2WithParam1 = visitor.Visit(expr2.Body);
-            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, body2WithParam1), parameter1);
+            ParameterExpression parameter = left.Parameters[0];
+            var visitor = new ReplaceParameterVisitor(right.Parameters[0], parameter);
+            var body = visitor.Visit(right.Body);
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, body), parameter);
         }
 
         private class ReplaceParameterVisitor : ExpressionVisitor
         {
-            private readonly ParameterExpression _oldParameter;
-            private readonly ParameterExpression _newParameter;
+            private readonly ParameterExpression _left;
+            private readonly ParameterExpression _right;
 
-            public ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+            public ReplaceParameterVisitor(ParameterExpression left, ParameterExpression right)
             {
-                _oldParameter = oldParameter;
-                _newParameter = newParameter;
+                _left = left;
+                _right = right;
             }
 
             protected override Expression VisitParameter(ParameterExpression node)
             {
-                if (ReferenceEquals(node, _oldParameter))
-                    return _newParameter;
+                if (ReferenceEquals(node, _left))
+                    return _right;
 
                 return base.VisitParameter(node);
             }
