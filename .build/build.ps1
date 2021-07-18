bc248a7a986ef8e9d46d57b28657070fef997ef9
efeozyer <efeozyer@yandex.com>
2021-07-18 22:22:43 +0300

    script changes

--- .build/build.ps1
+++ .build/build.ps1
@@ -9,14 +9,17 @@
 
 foreach ($rootDir in Get-ChildItem -Directory -Path . -Filter 'Eiffel.*')
 {
-    foreach ($subDir in Get-ChildItem -Path "$($rootDir.Name)\src\**\") 
+    foreach ($subDir in Get-ChildItem -Path "$($rootDir.FullName)\src\**\") 
     {
         $files = (& git diff $prevSHA $commitSHA --name-only $subDir.FullName "$($subDir.FullName)")
         if ($files.Count -gt 0) 
         {
             $csprojFile = "$($subDir.FullName)\$($subDir.Name).csproj"
+
             Write-Host "Build started $($subDir.FullName)\$($subDir.Name)"
+
             & dotnet build $csprojFile --no-restore
+
             if (-Not($?)) 
             {
                 throw "Build failed!"
